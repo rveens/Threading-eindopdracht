d5f666b4c0857aef6132ec4c67ba9222afb5b57f
Pascal <pascal270@gmail.com>
2013-05-30 22:30:15 +0200

    Added SettingsIOHandler which load the settings from settings.xml file into application

---
+++ src/nl/avans/threading/Settings.java
@@ -0,0 +1,21 @@
+package nl.avans.threading;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Pascal Slegtenhorst
+ * Date: 30-5-13
+ * Time: 20:50
+ */
+public class Settings {
+    //DB-settings//
+    public static String dbUrl = "jdbc:mysql://localhost:3306/";
+    public static String dbTable = "webservAuth";
+    public static String dbUsername = "root";
+    public static String dbPassword = "fDb4tcwT38Dd";
+
+    //Server-settings//
+    public static int webPort = 80;
+    public static int controlPort = 81;
+    public static String webRoot = "";
+    public static String defaultPage = "";
+}
