4602df64aecff43ff0672a395c005ceb58d5799d
rick <rickveens92@gmail.com>
2013-05-31 14:29:54 +0200

    Added RequestExecuter class

---
+++ src/nl/avans/threading/Requesthandling/RequestExecuter.java
@@ -0,0 +1,27 @@
+package nl.avans.threading.Requesthandling;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: rick
+ * Date: 5/31/13
+ * Time: 2:24 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class RequestExecuter
+{
+    public RequestExecuter(String method, String filepath, String[] body, String contentType)
+    {
+
+    }
+
+    private boolean executeGet()
+    {
+
+    }
+
+    private boolean executePost()
+    {
+
+    }
+}
+
