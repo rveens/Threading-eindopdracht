09b6a967a3ab7f82c0f717ff1739ad99f256b4ae
rick <rickveens92@gmail.com>
2013-05-31 14:05:45 +0200

    Added test response

--- src/nl/avans/threading/Requesthandling/RequestHandler.java
+++ src/nl/avans/threading/Requesthandling/RequestHandler.java
@@ -1,8 +1,12 @@
 package nl.avans.threading.Requesthandling;
 
+import nl.avans.threading.WebserverConstants;
+
+import java.io.DataOutputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.Socket;
+import java.util.Date;
 
 /**
  * Created with IntelliJ IDEA.
@@ -23,6 +27,7 @@ public class RequestHandler implements Runnable {
     @Override
     public void run() {
         try {
+            /* 1 - parsen van request */
             reqparser = new HTTPRequestParser(new InputStreamReader(sok.getInputStream()));
             try {
                 reqparser.validateRequest();
@@ -31,10 +36,50 @@ public class RequestHandler implements Runnable {
                 e.printStackTrace();
                 System.out.println("Invalid request");
             }
-            // Gebruik DataOutPutStream voor schrijven naar een socket.
+            /* 2 - bestand ophalen of reageren op POST */
+
+            /* 3 - response terugsturen */
+            sendResponse(200, reqparser.getHttpVersion());
 
             // Sluit de socket -> niet met http1.1
-            //sok.close();
+            sok.close();
+        } catch (IOException e) {
+            e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
+        }
+    }
+
+    private void sendResponse(int statusCode, int[] httpVersion)
+    {
+        String initialResponseLine, headers, body;
+        Date currentTime;
+
+        initialResponseLine = headers = body = "";
+        currentTime = new Date();
+
+        initialResponseLine += "HTTP/";
+        initialResponseLine += httpVersion[0];
+        initialResponseLine += ".";
+        initialResponseLine += httpVersion[1];
+        initialResponseLine += " ";
+        initialResponseLine += statusCode;
+        initialResponseLine += " ";
+        initialResponseLine += WebserverConstants.HttpReplies.get(statusCode);
+        initialResponseLine += '\n';
+
+        headers += "Date: " + WebserverConstants.DATE_FORMAT.format(currentTime) + '\n';
+        headers += "Server: SuperWebserver/0.1 (" + System.getProperty("os.name") + ")" + '\n';
+        headers += "Content-Type: text/html" + '\n';
+
+        headers += "\r\n";
+
+        try {
+            DataOutputStream out = new DataOutputStream(sok.getOutputStream());
+            out.writeBytes(initialResponseLine);
+            out.writeBytes(headers);
+            // TODO opgevraagde bestand lezen bij een GET
+            out.writeBytes("appelflap");
+
+            out.close(); // NIET VERGETEN
         } catch (IOException e) {
             e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
         }
