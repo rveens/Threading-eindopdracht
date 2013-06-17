5e0504c0b03cf7f4f11bc0482c2c6f7cdaee1c19
rick <rickveens92@gmail.com>
2013-06-17 14:20:20 +0200

    Fixed content-body URL decoding

--- src/nl/avans/threading/Requesthandling/HTTPRequestParser.java
+++ src/nl/avans/threading/Requesthandling/HTTPRequestParser.java
@@ -6,6 +6,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.URLDecoder;
+import java.net.URLEncoder;
 import java.util.Hashtable;
 
 /**
@@ -95,7 +96,7 @@ public class HTTPRequestParser {
                 char[] buffer = new char[Integer.parseInt((String)headers.get("content-length"))];
                 bufferedReader.read(buffer, 0, buffer.length);
                 try {
-                    parseContentbody(new String(buffer));
+                    parseContentbody(URLDecoder.decode(new String(buffer), "UTF-8"));
                 } catch (Exception e) {
                     System.out.println(e.getMessage());
                 }
